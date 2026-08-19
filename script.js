6e5e7c74e664cd440af01bd7f7c561743f8f34a8
Srilex Buditra <srilexbuditra@gmail.com>
2026-08-19 23:33:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const WA_NUMBER='6282135238350';
+const WA_NUMBER='6282136238350';
 const fmt=n=>new Intl.NumberFormat('id-ID').format(n);
 let base=5000000, extra=0, packageName='Professional';
 const $=s=>document.querySelector(s);
